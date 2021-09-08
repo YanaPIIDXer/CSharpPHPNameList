ba839feb914ad5752871d195acc975ed4e0485f4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-09 04:23:38 +0900

    消去ＡＰＩ定義

---
+++ server/src/api/delete.php
@@ -0,0 +1,19 @@
+<?php
+    $json = file_get_contents("php://input");
+    $contents = json_decode($json, true);
+    
+    $id = $contents["id"];
+    $result = ["result" => false];
+    if ($id === null)
+    {
+        echo json_encode($result);
+        return;
+    }
+    
+    $conn = new SQLConnection();
+    $stmt = $conn->query("DELETE FROM users WHERE id = :id;");
+    $stmt->bind(":id", $id, PDO::PARAM_INT);
+
+    $result["result"] = $stmt->execute();
+    echo json_encode($result);
+?>
