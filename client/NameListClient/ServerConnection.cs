1706bcefc7e79edd5598d7984da17c79de256697
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-09 04:32:53 +0900

    サーバ接続用クラスをとりあえず定義

---
+++ client/NameListClient/ServerConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameListClient
+{
+	/// <summary>
+	/// サーバ接続クラス
+	/// </summary>
+	public static class ServerConnection
+	{
+		/// <summary>
+		/// エンドポイントのフォーマット
+		/// ※string.Formatメソッドに投げるもの
+		/// </summary>
+		private static readonly string ENDPOINT_FORMAT = "http://localhost/api/{0}.php";
+	}
+}
