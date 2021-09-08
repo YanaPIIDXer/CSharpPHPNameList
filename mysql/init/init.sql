df3a149cda2e512de5de3c6f148d8c5c9b5eb289
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-09 03:32:30 +0900

    初期データを突っ込んでおいた

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -3,3 +3,7 @@ GRANT ALL ON name_list.* TO develop@'%' IDENTIFIED BY 'develop';
 
 USE name_list;
 CREATE TABLE users(id INTEGER PRIMARY KEY AUTO_INCREMENT, last_name VARCHAR(32), first_name VARCHAR(32));
+
+INSERT INTO users(last_name, first_name) VALUES("AAA", "AAA");
+INSERT INTO users(last_name, first_name) VALUES("BBB", "BBB");
+INSERT INTO users(last_name, first_name) VALUES("CCC", "CCC");
